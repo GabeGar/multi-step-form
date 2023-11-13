86a37ac1f72b54c281d8fed97ee7be49b2d149d9
Gabegar <gagarcia190@gmail.com>
2023-11-12 22:20:16 -0500

    minor style fix/update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MultiStepFormActions from './components/MultiStepForm/MultiStepFormAction
 
 const App = () => {
     return (
-        <div className="bg-sideBarMobile flex min-h-[100dvh] flex-col items-center gap-6 bg-primary-light-blue/40 bg-contain bg-no-repeat">
+        <div className="flex min-h-[100dvh] flex-col items-center justify-center gap-6 bg-primary-light-blue/40 bg-sideBarMobile bg-contain bg-no-repeat">
             <MultiStepIndicator />
             <MultiStepForm />
             <MultiStepFormActions />
--- src/components/MultiStepForm/StepOne/StepOneRow.tsx
+++ src/components/MultiStepForm/StepOne/StepOneRow.tsx
@@ -7,8 +7,10 @@ interface Props {
 
 const StepOneRow = ({ label, htmlFor, error, children }: Props) => {
     return (
-        <div className="text-primary-marine-blue">
-            <label htmlFor={htmlFor}>{label}</label>
+        <div className="flex flex-col text-primary-marine-blue">
+            <label htmlFor={htmlFor} className="flex-1">
+                {label}
+            </label>
             {children}
             {error && (
                 <p className="font-bold text-primary-strawberry-red">{error}</p>
